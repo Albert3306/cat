11740e2b97c069b701561890df7032a1a1d603df
Albert3306 <wujipeng@aliyun.com>
2017-06-05 14:39:34 +0800

    ++

---
+++ resources/views/admin/layout/_base.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>@section('title') CAT - ALBERT3306 @show{{-- 页面标题 --}}</title>
+    <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">{{-- favicon --}}
+
+    @section('head_css')
+    @show{{-- head区域css样式表 --}}
+
+    @section('head_js')
+    @show{{-- head区域javscript脚本 --}}
+</head>
+<body>
+
+    @section('body')
+    @show{{-- 正文部分 --}}
+
+</body>
+</html>
