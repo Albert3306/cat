fd4fa90d8e1d171f692b9c55a18166016e9893ff
wujipeng <albert3306@aliyun.com>
2017-08-25 13:37:21 +0800

    ++

--- resources/views/admin/layout/_base.blade.php
+++ resources/views/admin/layout/_base.blade.php
@@ -14,7 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@section('title') CAT - ALBERT3306 @show{{-- 页面标题 --}}</title>
-    <link rel="shortcut icon" href="/back/images/favicon.ico" type="image/x-icon">{{-- favicon --}}
+    <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">{{-- favicon --}}
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css">
--- resources/views/desktop/index.blade.php
+++ resources/views/desktop/index.blade.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>welcome</title>
+    <title>@section('title') CAT - ALBERT3306 @show{{-- 页面标题 --}}</title>
+    <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">{{-- favicon --}}
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 </head>
 <body>
     <div style="width: 1000px; height: 900px; margin: 0 auto; text-align: center;">
